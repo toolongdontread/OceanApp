d855fd714f9d9d3c68801c6651f2a92ae77cc18c
toolongdontread <kcltommy01@gmail.com>
2022-11-15 18:19:57 +0800

    update hunting shark

--- app/src/main/java/com/example/oceanapp/huntingshark.java
+++ app/src/main/java/com/example/oceanapp/huntingshark.java
@@ -1,14 +1,33 @@
 package com.example.oceanapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class huntingshark extends AppCompatActivity {
+
+    private Button ytlink;
+    private Button sharkfinbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_huntingshark);
+
+        ytlink = findViewById(R.id.ytlink);
+        ytlink.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Uri uri = Uri.parse("https://www.youtube.com/watch?v=7iIyKPpII20&ab_channel=AdventuresofTwoAfloat");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
     }
 }
